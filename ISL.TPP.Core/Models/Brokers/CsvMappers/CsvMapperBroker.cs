49685315be0d40842bf7ed1e13dae71b1941ab18
Christo du Toit <christo@dutoit.co.uk>
2024-02-07 12:39:24 +0000

    BROKERS:  CSV Mapper

---
+++ ISL.TPP.Core/Models/Brokers/CsvMappers/CsvMapperBroker.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace ISL.TPP.Core.Models.Brokers.CsvMappers
+{
+    public class CsvMapperBroker : ICsvMapperBroker
+    {
+        public async ValueTask<List<T>> MapCsvToObjectAsync<T>(string data, bool hasHeaderRecord)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = hasHeaderRecord,
+                MissingFieldFound = null
+            };
+
+            using (var reader = new StringReader(data))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var records = csv.GetRecords<T>().ToList();
+                return await ValueTask.FromResult(records);
+            }
+        }
+
+        public async ValueTask<string> MapObjectToCsvAsync<T>(
+            List<T> @object,
+            bool addHeaderRecord,
+            bool shouldAddTrailingComma)
+        {
+            var csvWriterConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = addHeaderRecord,
+                NewLine = Environment.NewLine
+            };
+
+            using (var stringWriter = new StringWriter())
+            using (var csvWriter = new CsvWriter(stringWriter, csvWriterConfig))
+            {
+                if (addHeaderRecord)
+                {
+                    csvWriter.WriteHeader<T>();
+                    csvWriter.NextRecord();
+                }
+
+                foreach (var item in @object)
+                {
+                    csvWriter.WriteRecord(item);
+                    if (shouldAddTrailingComma) { csvWriter.WriteField(""); }
+                    csvWriter.NextRecord();
+                }
+
+                var csv = stringWriter.ToString();
+
+                return await ValueTask.FromResult(csv);
+            }
+        }
+    }
+}
