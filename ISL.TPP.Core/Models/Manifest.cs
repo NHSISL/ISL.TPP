5a52f300a60beb026a6190530485191c0d3a2ed6
Christo du Toit <christo@dutoit.co.uk>
2024-02-07 13:45:54 +0000

    FOUNDATIONS: CSV Mapper

--- ISL.TPP.Core/Models/Manifest.cs
+++ ISL.TPP.Core/Models/Manifest.cs
@@ -6,10 +6,10 @@ namespace ISL.TPP.Core.Models
 {
     internal class Manifest
     {
-        public string FileName { get; set; }
-        public string IsDelta { get; set; }
-        public string IsReference { get; set; }
-        public string DateExtractFrom { get; set; }
-        public string DateExtractTo { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string IsDelta { get; set; } = string.Empty;
+        public string IsReference { get; set; } = string.Empty;
+        public string DateExtractFrom { get; set; } = string.Empty;
+        public string DateExtractTo { get; set; } = string.Empty;
     }
 }
