af45637cf21bcf23a9596dc4089980777204f767
Christo du Toit <christo@dutoit.co.uk>
2024-02-08 14:19:27 +0000

    ShouldMoveToFileToDestinationIfFolderExistsAsync -> PASS

--- ISL.TPP.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Logic.MoveFile.cs
+++ ISL.TPP.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Logic.MoveFile.cs
@@ -59,5 +59,46 @@ namespace ISL.TPP.Core.Tests.Unit.Services.Foundations.Files
 
             this.fileBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldMoveToFileToDestinationIfFolderExistsAsync()
+        {
+            // given
+            string sourcePath = "C:\\Temp\\Folder1";
+            string destinationPath = "C:\\Temp\\Folder2";
+            string fileName = $"{GetRandomString()}.txt";
+            string fullSourcePath = Path.Combine(sourcePath, fileName);
+            string fullDestinationPath = Path.Combine(destinationPath, fileName);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.GetDirectoryAsync(fullDestinationPath))
+                    .ReturnsAsync(destinationPath);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.CheckIfDirectoryExistsAsync(destinationPath))
+                    .ReturnsAsync(true);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.MoveFileAsync(fullSourcePath, fullDestinationPath))
+                    .ReturnsAsync(true);
+
+            // when
+            await this.fileService.MoveFileAsync(fullSourcePath, fullDestinationPath);
+
+            // then
+            this.fileBrokerMock.Verify(broker =>
+                broker.GetDirectoryAsync(fullDestinationPath),
+                    Times.Once);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.CheckIfDirectoryExistsAsync(destinationPath),
+                    Times.Once);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.MoveFileAsync(fullSourcePath, fullDestinationPath),
+                    Times.Once);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
