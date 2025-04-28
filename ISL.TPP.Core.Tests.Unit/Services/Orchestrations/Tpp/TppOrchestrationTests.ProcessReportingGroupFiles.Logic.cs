8a0c8385aa59684863bb8c40819c5ea6bcf28cc4
Christo du Toit <christo@dutoit.co.uk>
2025-04-28 15:00:12 +0100

    ShouldNotProcessReportingGroupFilesWhenManifestDontExistAsync -> PASS

--- ISL.TPP.Core.Tests.Unit/Services/Orchestrations/Tpp/TppOrchestrationTests.ProcessReportingGroupFiles.Logic.cs
+++ ISL.TPP.Core.Tests.Unit/Services/Orchestrations/Tpp/TppOrchestrationTests.ProcessReportingGroupFiles.Logic.cs
@@ -2,7 +2,10 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using ISL.TPP.Core.Services.Orchestrations.Tpp;
+using Moq;
 using Xunit;
 
 namespace ISL.TPP.Core.Tests.Unit.Services.Orchestrations.Tpp
@@ -10,7 +13,49 @@ namespace ISL.TPP.Core.Tests.Unit.Services.Orchestrations.Tpp
     public partial class TppOrchestrationTests
     {
         [Fact]
-        public async Task ShouldProcessReportingGroupFilesAsync()
+        public async Task ShouldNotProcessReportingGroupFilesWhenManifestDontExistAsync()
+        {
+            // given
+            string randomReportingGroupFolder = GetRandomString();
+            string manifestFile = tppConfiguration.TppManifestFile;
+            List<string> randomFiles = GetRandomStringList(GetRandomNumber());
+
+            this.fileServiceMock.Setup(service =>
+                service.RetrieveListOfFilesAsync(randomReportingGroupFolder, "*"))
+                    .ReturnsAsync(randomFiles);
+
+            var tppOrchestrationServiceMock = new Mock<TppOrchestrationService>(
+                this.fileServiceMock.Object,
+                this.documentServiceMock.Object,
+                this.csvMapperServiceMock.Object,
+                this.tppConfiguration,
+                this.dateTimeBrokerMock.Object,
+                this.loggingBrokerMock.Object)
+            {
+                CallBase = true
+            };
+
+            // when
+            await tppOrchestrationServiceMock.Object.ProcessReportingGroupFilesAsync(randomReportingGroupFolder);
+
+            // then
+            this.fileServiceMock.Verify(service =>
+                service.RetrieveListOfFilesAsync(randomReportingGroupFolder, "*"),
+                    Times.Once);
+
+            tppOrchestrationServiceMock.Verify(service =>
+                service.ProcessReportingGroupFilesAsync(randomReportingGroupFolder),
+                    Times.Once);
+
+            tppOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.fileServiceMock.VerifyNoOtherCalls();
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldProcessReportingGroupFilesWhenManifestExistAsync()
         {
             //// given
             //var tppOrchestrationServiceMock = new Mock<TppOrchestrationService>(
