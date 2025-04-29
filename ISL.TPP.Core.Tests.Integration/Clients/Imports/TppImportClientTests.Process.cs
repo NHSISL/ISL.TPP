c0c91ab61bc031f7cf176f0223b2b4c1a499cf6e
Christo du Toit <christo@dutoit.co.uk>
2025-04-29 13:21:16 +0100

    ShouldProcessNewFilesFromreprocessFolderAsync -> PASS

--- ISL.TPP.Core.Tests.Integration/Clients/Imports/TppImportClientTests.Process.cs
+++ ISL.TPP.Core.Tests.Integration/Clients/Imports/TppImportClientTests.Process.cs
@@ -14,7 +14,7 @@ namespace ISL.TPP.Core.Tests.Integration.Clients.Imports
     public partial class TppImportClientTests
     {
         [ReleaseCandidateFact]
-        public async Task ShouldProcessNewFilesIfManifestFilePresentAsync()
+        public async Task ShouldProcessNewFilesFromRootAsync()
         {
             // given
             string randomFileName = $"{GetRandomString()}.txt";
@@ -26,6 +26,50 @@ namespace ISL.TPP.Core.Tests.Integration.Clients.Imports
             foreach (string reportingGroup in this.tppConfiguration.ReportingGroups)
             {
                 string reportingGroupFolder = Path.Combine(this.tppConfiguration.TppPickupFolder, reportingGroup);
+                string manifestFile = Path.Combine(reportingGroupFolder, this.tppConfiguration.TppManifestFile);
+                string randomFile = Path.Combine(reportingGroupFolder, $"{GetRandomString(1)}.txt");
+
+                if (!Directory.Exists(reportingGroupFolder))
+                {
+                    Directory.CreateDirectory(reportingGroupFolder);
+                }
+
+                foreach (string file in Directory.EnumerateFiles(reportingGroupFolder))
+                {
+                    File.Delete(file);
+                    Console.WriteLine($"File '{file}' deleted.");
+                }
+
+                File.WriteAllText(manifestFile, csvManifest.ToString());
+                File.WriteAllText(randomFile, randomFileName);
+            }
+
+            TppClient client = new TppClient(tppConfiguration);
+
+            // when
+            await client.Imports.ProcessFilesAsync();
+
+            // then
+        }
+
+        [ReleaseCandidateFact]
+        public async Task ShouldProcessNewFilesFromreprocessFolderAsync()
+        {
+            // given
+            string randomFileName = $"{GetRandomString()}.txt";
+            string manifestToDate = DateTime.Now.ToString("yyyyMMdd_HHmm");
+            StringBuilder csvManifest = new StringBuilder();
+            csvManifest.AppendLine("FileName,IsDelta,IsReference,DateExtractFrom,DateExtractTo");
+            csvManifest.AppendLine($"{randomFileName},Y,N,20231209_2245,{manifestToDate}");
+
+            foreach (string reportingGroup in this.tppConfiguration.ReportingGroups)
+            {
+                string reportingGroupFolder = Path.Combine(
+                    this.tppConfiguration.TppPickupFolder,
+                    reportingGroup,
+                    this.tppConfiguration.TppWorkingFolders.ReProcess,
+                    manifestToDate);
+
                 string manifestFile = Path.Combine(reportingGroupFolder, this.tppConfiguration.TppManifestFile);
                 string randomFile = Path.Combine(reportingGroupFolder, $"{GetRandomString(1)}.txt");
 
