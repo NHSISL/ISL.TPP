5424437995b1e64024ac83fc78b4a39fde40cd77
Christo du Toit <christo.dutoit@nhs.net>
2025-06-24 15:12:57 +0100

    ShouldThrowValidationExceptionOnMoveFileIfSourceFileDoesNotExistsAsync -> PASS

--- ISL.TPP.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Validations.MoveFile.cs
+++ ISL.TPP.Core.Tests.Unit/Services/Foundations/Files/FileServiceTests.Validations.MoveFile.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.IO;
 using System.Threading.Tasks;
 using FluentAssertions;
 using ISL.TPP.Core.Models.Foundations.Files.Exceptions;
@@ -51,6 +52,70 @@ namespace ISL.TPP.Core.Tests.Unit.Services.Foundations.Files
 
             this.fileBrokerMock.Verify(broker =>
                 broker.MoveFileAsync(invalidSourcePath, invalidDestinationPath),
+                    Times.Never);
+
+            this.fileBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnMoveFileIfSourceFileDoesNotExistsAsync()
+        {
+            // given
+            string sourcePath = "C:\\Temp\\Folder1";
+            string destinationPath = "C:\\Temp\\Folder2";
+            string fileName = $"{GetRandomString()}.txt";
+            string fullSourcePath = Path.Combine(sourcePath, fileName);
+            string fullDestinationPath = Path.Combine(destinationPath, fileName);
+
+            var invalidArgumentFileException =
+                new InvalidArgumentFileException(
+                    message: "Invalid file argument(s), please correct the errors and try again.");
+
+            invalidArgumentFileException.AddData(
+                key: "SourcePath",
+                values: $"File does not exist. Filename: {fullSourcePath}");
+
+            var expectedFileValidationException =
+                new FileValidationException(
+                    message: "File validation error occurred, fix the errors and try again.",
+                    innerException: invalidArgumentFileException);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.GetDirectoryAsync(fullDestinationPath))
+                    .ReturnsAsync(destinationPath);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.CheckIfDirectoryExistsAsync(destinationPath))
+                    .ReturnsAsync(false);
+
+            this.fileBrokerMock.Setup(broker =>
+                broker.CheckIfFileExistsAsync(fullSourcePath))
+                    .ReturnsAsync(false);
+
+            // when
+            ValueTask<bool> moveFileTask =
+                this.fileService.MoveFileAsync(fullSourcePath, fullDestinationPath);
+
+            FileValidationException actualException =
+                await Assert.ThrowsAsync<FileValidationException>(moveFileTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedFileValidationException);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.GetDirectoryAsync(fullDestinationPath),
+                    Times.Once);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.CheckIfDirectoryExistsAsync(destinationPath),
+                    Times.Once);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.CheckIfFileExistsAsync(fullSourcePath),
+                    Times.Once);
+
+            this.fileBrokerMock.Verify(broker =>
+                broker.MoveFileAsync(fullSourcePath, fullDestinationPath),
                     Times.Never);
 
             this.fileBrokerMock.VerifyNoOtherCalls();
